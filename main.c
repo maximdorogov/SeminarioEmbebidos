bce81ce79c0ea84a4994f88aac0206daf4e28ffa
maximdorogov <maxim_dorogov@yahoo.com>
2018-10-06 20:25:13 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #define TP1_1 (1)
 #define TP1_2 (2)
 #define TP1_3 (3)
-#define TEST (TP1_1)
+#define TP1_4 (4)
+#define TEST (TP1_4)
 
 
 
@@ -129,3 +130,78 @@ void myTickHook( void *ptr ){
 			}
 
 #endif
+
+#if (TEST == TP1_4)
+
+#define TICKRATE_MS	50
+#define LED_TOGGLE_MS 500
+
+
+void myTickHook( void *);
+bool_t MytickConfig( tick_t );
+
+int main(void){
+	   /* ------------- INICIALIZACIONES ------------- */
+
+	   /* Inicializar la placa */
+	   boardConfig();
+	   DEBUG_PRINT_ENABLE;
+	   debugPrintConfigUart( UART_USB, 115200 );
+
+	   /* Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
+	      periodicamente una interrupcion cada 50ms que incrementa un contador de
+	      Ticks obteniendose una base de tiempos). */
+	   tickConfig(  TICKRATE_MS	 );
+
+	   /* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
+	      simplemente una funcion que se ejecutara peri�odicamente con cada
+	      interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
+	      a una interrupcion.
+	      El segundo parametro es el parametro que recibe la funcion myTickHook
+	      al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
+	   */
+	   tickCallbackSet( myTickHook, (void*)LEDR );
+	   delay(LED_TOGGLE_MS);
+
+	   debugPrintString( "DEBUG c/sAPI\r\n" );
+	   debugPrintString( "LED Toggle\n" );
+	   /* ------------- REPETIR POR SIEMPRE ------------- */
+	   while(1) {
+	      tickCallbackSet( myTickHook, (void*)LEDG );
+	      delay(LED_TOGGLE_MS);
+	      tickCallbackSet( myTickHook, (void*)LEDB );
+	      delay(LED_TOGGLE_MS);
+	      tickCallbackSet( myTickHook, (void*)LED1 );
+	      delay(LED_TOGGLE_MS);
+	      tickCallbackSet( myTickHook, (void*)LED2 );
+	      delay(LED_TOGGLE_MS);
+	      tickCallbackSet( myTickHook, (void*)LED3 );
+	      delay(LED_TOGGLE_MS);
+	      tickCallbackSet( myTickHook, (void*)LEDR );
+	      delay(LED_TOGGLE_MS);
+	   }
+
+	   return 0 ;
+}
+
+void myTickHook( void *ptr ){
+
+				static bool_t ledState = OFF;
+
+				gpioMap_t led = (gpioMap_t)ptr;
+
+				if( ledState ){
+					ledState = OFF;
+				}
+				else{
+					ledState = ON;
+				}
+				gpioWrite( led, ledState );
+			}
+
+#endif
+
+
+/* UART for debug messages. */
+
+
